89e1a24eb4d3d7e14abdf41f89a5fa76c062b617
Ayuquina_Angel <aayuquina422@uta.edu.ec>
2024-11-22 13:39:25 -0500

    Archivos del proyecto Blog

---
+++ blogging/project/components/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+   $db_name = 'mysql:host=localhost;dbname=blogg';
+   $user_name = 'root';
+   $user_password = 'Angel_4220';
+
+   $conn = new PDO($db_name, $user_name, $user_password);
+
+?>
